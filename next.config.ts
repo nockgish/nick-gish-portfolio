e8f97780a191bc7b1f7033d6bdd44bb69eaa3ad8
nockgish <nicholas.gish@gmail.com>
2026-04-23 21:59:24 -0500

    mp3 filepath correction

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,11 @@ const nextConfig: NextConfig = {
         destination:
           "https://llqptt7vz21n4gjy.public.blob.vercel-storage.com/images/:path*",
       },
+      {
+        source: "/mp3s/:path*",
+        destination:
+          "https://llqptt7vz21n4gjy.public.blob.vercel-storage.com/mp3s/:path*",
+      },
     ];
   },
 };
